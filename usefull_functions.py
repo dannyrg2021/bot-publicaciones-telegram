88821c2f1c78dbab79ea0dec73df480ca02e4b31
Reima Hopper <reinier.mayea@nauta.cu>
2025-02-16 06:07:02 -0500

    loalsd

--- usefull_functions.py
+++ usefull_functions.py
@@ -956,7 +956,8 @@ def operaciones_DB(call, bot, host_url, operacion , archivo=False, id=False):
         for diccionario in lista:
             
 
-            bot.send_document(call.message.chat.id, diccionario["archivo"], caption=f"ID de archivo: {diccionario['_id']}\n\n<u>Fecha Creación</u>:\n{time.strftime('<b>Hora</b>: %H:%M %p\n<b>Fecha</b>: %d/%m/%Y', time.localtime(calcular_diferencia_horaria(diccionario['fecha'])))}", reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Cargar Archivo 💫", callback_data=f"db_cargar:{diccionario["_id"]}")], [InlineKeyboardButton("Eliminar Archivo 💥", callback_data=f"db_eliminar:{diccionario["_id"]}")]]))
+            bot.send_document(call.message.chat.id, diccionario["archivo"], caption="ID de archivo: {}\n\n<u>Fecha Creación</u>:\n{}".format(diccionario['_id'], time.strftime('<b>Hora</b>: %H:%M %p\n<b>Fecha</b>: %d/%m/%Y', time.localtime(calcular_diferencia_horaria(diccionario['fecha'])))), reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Cargar Archivo 💫", callback_data=f"db_cargar:{diccionario["_id"]}")], [InlineKeyboardButton("Eliminar Archivo 💥", callback_data=f"db_eliminar:{diccionario["_id"]}")]]))
+            
             
         return
             
