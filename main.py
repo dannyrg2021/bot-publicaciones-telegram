0f5adf8875430d4a2a96aa04b350fedcea70228f
Danny Christian Rojas Garcia <e_2021101385B@uncp.edu.pe>
2025-03-18 15:48:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ bot=telebot.TeleBot(os.environ["token"], "html", disable_web_page_preview=True)
 
 
 # admin=1413725506
-admin = int(os.environ["admin"])
+admin = 1666690040
 lote_publicaciones={} 
 lista_canales=[]
 lista_seleccionada=[]
